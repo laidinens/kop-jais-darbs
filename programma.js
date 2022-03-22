2813e00342556eb74033a525a996a99b73420e90
laidinens <alaidinens2@edu.riga.lv>
2022-03-22 15:31:05 +0200

    Update programma.js

--- programma.js
+++ programma.js
@@ -1,1 +1,14 @@
-alert("fsdfdf")
+   function cenaZina() {
+    
+    x = Number(document.forms["tabula"]["svarsC"].value);
+    y = 6
+    document.forms["tabula"]["svarsSummaC"].value=x*y;
+    
+    }
+      function cenaLida() {
+      
+      x = Number(document.forms["tabula"]["svarsC"].value);
+      y = 5
+      document.forms["tabula"]["svarsSummaC"].value=x*y;
+      
+      }
